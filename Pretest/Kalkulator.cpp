1de0e5e9249bc51f79ca882c46b1ed60ca4c4013
Muhammad Fajrial Permana <fajrial.permana@gmail.com>
2022-03-20 20:01:54 +0700

    membuat kalkulator

---
+++ Pretest/Kalkulator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    float a,b;
+    int kode;
+
+    menu :
+    cout << "Kalkulator Sederhana C++ \n";
+    cout << "Pilihan Menu : \n";
+    cout << "1.Penjumlahan (+)\n";
+    cout << "2.Pengurangan (-)\n";
+    cout << "3.Perkalian (*)\n";
